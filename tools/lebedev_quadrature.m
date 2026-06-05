8f506c43ce028caa31ac7b8cb077d59ee3d79267
Chongran-Zhao <imchongran@gmail.com>
2026-06-05 21:32:11 +0800

    update Lebedev quadrature rule

--- tools/lebedev_quadrature.m
+++ tools/lebedev_quadrature.m
@@ -1,10 +1,16 @@
 function out = lebedev_quadrature(fun)
+% Lebedev quadrature on the unit sphere.
+%
+% The data file is the 110-point, precision-17 Lebedev rule from:
+%   https://people.math.sc.edu/Burkardt/datasets/sphere_lebedev_rule/lebedev_017.txt
+% Its first two columns are angles in degrees and the weights are
+% normalized so that sum(weight) = 1.
 srcDir = fileparts(mfilename('fullpath'));
 rootDir = fileparts(srcDir);
 data = load(fullfile(rootDir, 'data', 'Lebedev.txt'));
 
-phi = data(:, 1);
-theta = data(:, 2);
+phi = deg2rad(data(:, 1));
+theta = deg2rad(data(:, 2));
 weight = data(:, 3);
 
 out = fun(theta(1), phi(1)) .* weight(1);
